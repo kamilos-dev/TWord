112b6b204f720e35eca19f5a23484369ea9e3e36
Szyn33k <grand.hernes@gmail.com>
2021-01-15 21:31:25 +0100

    Add comments in AtWordBuilder

--- src/TWord/AtWordBuilder.cs
+++ src/TWord/AtWordBuilder.cs
@@ -1,18 +1,25 @@
 ﻿namespace TWord
 {
+    /// <summary>
+    /// Builder for IAtWord instance
+    /// </summary>
     public class AtWordBuilder
     {
-        private Language _language;
-
-        private CurrencySymbol _currencySymbol;
-
         private readonly CurrencyOptions _currencyOptions;
+
+        private Language _language;
+        private CurrencySymbol _currencySymbol;
 
         public AtWordBuilder()
         {
             _currencyOptions = new CurrencyOptions();
         }
 
+        /// <summary>
+        /// Set language
+        /// </summary>
+        /// <param name="language">Language symbol</param>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder SetLanguage(Language language)
         {
             _language = language;
@@ -20,6 +27,11 @@
             return this;
         }
 
+        /// <summary>
+        /// Set currency
+        /// </summary>
+        /// <param name="currencySymbol">Currency symbol</param>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder SetCurrency(CurrencySymbol currencySymbol)
         {
             _currencySymbol = currencySymbol;
@@ -27,6 +39,10 @@
             return this;
         }
 
+        /// <summary>
+        /// Transform number to words without decimal part
+        /// </summary>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder IntegerPartOnly()
         {
             _currencyOptions.IntegerPartOnly = true;
@@ -34,6 +50,10 @@
             return this;
         }
 
+        /// <summary>
+        /// Returns decimal part as fraction e.g 5/100
+        /// </summary>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder DecimalPartAsFraction()
         {
             _currencyOptions.DecimalAsFraction = true;
@@ -41,6 +61,11 @@
             return this;
         }
 
+        /// <summary>
+        /// Hides subunit. For example 1.05 gives 
+        /// one dollar five
+        /// </summary>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder HideSubunit()
         {
             _currencyOptions.HideSubunit = true;
@@ -48,6 +73,10 @@
             return this;
         }
 
+        /// <summary>
+        /// Set separator for integer and decimal part
+        /// </summary>
+        /// <returns>AtWordBuilder instance</returns>
         public AtWordBuilder IntegerAndDecimalPartSeparator(string separator)
         {
             _currencyOptions.IntegerAndDecimalPartSeparator = separator;
@@ -55,6 +84,9 @@
             return this;
         }
 
+        /// <summary>
+        /// Returns IAtWord instance
+        /// </summary>
         public IAtWord Build()
         {
             return new AtWord(_language, _currencySymbol, _currencyOptions);
